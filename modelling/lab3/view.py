2339c21552c7e0d828a7b0f6605cbb92cc675056
sanyash <sanyash@yandex-team.ru>
2018-11-04 16:38:14 +0300

    improve modelling lab3

--- modelling/lab3/view.py
+++ modelling/lab3/view.py
@@ -24,33 +24,45 @@ class View(QWidget):
             'Лабораторная работа №3 по моделированию. '
             'Шилов ИУ7-72'
         )
-        self.setGeometry(600, 300, 800, 500)
+        self.setGeometry(600, 300, 1000, 500)
 
         self.n = 10
         self.matrix = [
             [
-                random.random() / 10 if i != j else 0 for j in range(self.n)
+                random.random() if i != j else 0.0 for j in range(self.n)
             ]
             for i in range(self.n)
         ]
+        self.start_probabilities = [
+            1 for i in range(self.n)
+        ]
+
+        self.p_lbl = QLabel('относительная\nначальная\nвероятность', self)
+        self.p_lbl.setGeometry(50, 0, 200, 50)
 
         self.matrix_lbl = QLabel('матрица интенсивности', self)
         self.matrix_lbl.setGeometry(400, 0, 360, 50)
 
         self.matrix_table = QTableWidget(self)
         self.result_table = QTableWidget(self)
+        self.p_table = QTableWidget(self)
         self.fill_tables()
+
+        # self.p_table = QTableWidget(self)
+        # self.p_table.setColumnCount(1)
+
+        self.calc()
 
         # == #
 
         self.n_edit = QLineEdit(str(self.n), self)
-        self.n_edit.setGeometry(75, 50, 50, 50)
+        self.n_edit.setGeometry(875, 50, 50, 50)
         self.n_btn = QPushButton('Изменить\nразмерность', self)
-        self.n_btn.setGeometry(25, 100, 150, 50)
+        self.n_btn.setGeometry(825, 100, 150, 50)
         self.n_btn.clicked.connect(self.resize_matrix)
 
         self.calc_btn = QPushButton('Рассчитать', self)
-        self.calc_btn.setGeometry(25, 200, 150, 50)
+        self.calc_btn.setGeometry(825, 200, 150, 50)
         self.calc_btn.clicked.connect(self.calc)
 
     def start_application(self):
@@ -59,8 +71,12 @@ class View(QWidget):
         self.qapp.exec_()
 
     def fill_tables(self):
+        start_probabilities = [
+            p / sum(self.start_probabilities)
+            for p in self.start_probabilities
+        ]
         self.limit_probabilities = matrix.calc_limit_probabilities(self.matrix)
-        self.stabilization_times = matrix.calc_stabilization_times(self.matrix, self.limit_probabilities)
+        self.stabilization_times = matrix.calc_stabilization_times(self.matrix, start_probabilities, self.limit_probabilities)
 
         self.matrix_table.setGeometry(200, 50, 594, 323)
         self.matrix_table.setColumnCount(self.n)
@@ -69,6 +85,11 @@ class View(QWidget):
         )
         for i in range(self.n):
             self.matrix_table.setColumnWidth(i, 50)
+        self.matrix_table.setRowCount(0)
+        for i in range(self.n):
+            self.matrix_table.insertRow(i)
+            for j in range(self.n):
+                self.matrix_table.setItem(i, j, Item(str(self.matrix[i][j])[:6], editable=(i != j)))
 
         self.result_table.setGeometry(10, 400, 785, 97)
         self.result_table.setColumnCount(self.n + 1)
@@ -78,15 +99,7 @@ class View(QWidget):
         self.result_table.setColumnWidth(0, 200)
         for i in range(self.n):
             self.result_table.setColumnWidth(i + 1, 50)
-
-        self.matrix_table.setRowCount(0)
         self.result_table.setRowCount(0)
-
-        for i in range(self.n):
-            self.matrix_table.insertRow(i)
-            for j in range(self.n):
-                self.matrix_table.setItem(i, j, Item(str(self.matrix[i][j])[:6], editable=(i != j)))
-
         self.result_table.insertRow(0)
         self.result_table.insertRow(1)
         self.result_table.setItem(0, 0, Item('Предельная вероятность'))
@@ -95,6 +108,17 @@ class View(QWidget):
             self.result_table.setItem(0, i + 1, Item(str(self.limit_probabilities[i])[:6]))
             self.result_table.setItem(1, i + 1, Item(str(self.stabilization_times[i])[:6]))
 
+        self.p_table.setGeometry(50, 50, 100, 323)
+        self.p_table.setColumnCount(1)
+        self.p_table.setHorizontalHeaderLabels(
+            ['']
+        )
+        self.p_table.setColumnWidth(0, 77)
+        self.p_table.setRowCount(0)
+        for i in range(self.n):
+            self.p_table.insertRow(i)
+            self.p_table.setItem(i, 0, Item(str(self.start_probabilities[i])[:6], editable=True))
+
     def resize_matrix(self):
         try:
             self.n = int(self.n_edit.text())
@@ -102,7 +126,7 @@ class View(QWidget):
                 raise RuntimeError
             new_matrix = [
                 [
-                    random.random() if i != j else 0 for j in range(self.n)
+                    random.random() if i != j else 0.0 for j in range(self.n)
                 ]
                 for i in range(self.n)
             ]
@@ -110,6 +134,10 @@ class View(QWidget):
                 for j in range(min(self.n, len(self.matrix))):
                     new_matrix[i][j] = self.matrix[i][j]
             self.matrix = new_matrix
+            self.start_probabilities = (
+                self.start_probabilities[:self.n] +
+                [0 for i in range(self.n - len(self.start_probabilities))]
+            )
             self.fill_tables()
         except Exception as exc:
             print(exc)
@@ -121,6 +149,10 @@ class View(QWidget):
                     float(self.matrix_table.item(i, j).text())
                     for j in range(self.n)
                 ]
+                for i in range(self.n)
+            ]
+            self.start_probabilities = [
+                float(self.p_table.item(i, 0).text())
                 for i in range(self.n)
             ]
             self.fill_tables()
